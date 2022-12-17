68b271f49f81403ad3fce903db272aba77250285
Weslley Xavier <fxavierweslley2016@gmail.com>
2022-12-16 23:19:42 -0300

    layout changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,27 @@ export class HomePage {
 
   constructor() {}
 
+  public accountOptions : Array<any> =[
+    { icon: "apps", text: "Área Pix"},
+    { icon: "barcode", text: "Pagar"},
+    { icon: "add-circle", text: "Pegar emprestado"},
+    { icon: "arrow-down", text: "Transferir"},
+    { icon: "arrow-up", text: "Depositar"},
+    { icon: "phone-portrait", text: "Recarga de celular"},  
+    { icon: "cash", text: "Cobrar"}, 
+    { icon: "heart-outline", text: "Doação"},  
+    { icon: "globe", text: "Transferir Internac."}, 
+    { icon: "trending-up", text: "Investir"},  
+    { icon: "logo-dropbox", text: "Caixinhas"}
+  ]; 
+
+  public slidesOptions: any = { slidesPerView: 4.5, freeMode: true} 
+
+  public slidehelpersOptions: any = { slidesPerView: 1.5, freeMode: true} 
+
+  public helpers : Array<any> =[
+    {text: "Taxas menores que 0,99% em remessas internacionais"},
+    {text: "Você tem até R$ 5.000,00 disponíveis para empréstimo."},
+    {text: "Convide amigos para o Nubank e desbloqueie braões incríveis."}
+  ]
 }
